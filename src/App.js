b411686ec4f508f4ea0c79493637ea73fec8243b
siam-26 <siamshahriar390@gmail.com>
2023-04-10 15:50:17 +0600

    navbar style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <h1>g</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,109 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+            <Nav>
+                <Logo src='/images/logo.svg'/>
+
+                <NavMenu>
+                    <a>
+                        <img src='/images/home-icon.svg'/>
+                        <span>Home</span>
+                    </a>
+
+                    <a>
+                        <img src='/images/search-icon.svg'/>
+                        <span>Search</span>
+                    </a>
+
+                    <a>
+                        <img src='/images/watchlist-icon.svg'/>
+                        <span>WatchList</span>
+                    </a>
+
+                    <a>
+                        <img src='/images/original-icon.svg'/>
+                        <span>Originals</span>
+                    </a>
+
+                    <a>
+                        <img src='/images/movie-icon.svg'/>
+                        <span>Movies</span>
+                    </a>
+
+                    <a>
+                        <img src='/images/series-icon.svg'/>
+                        <span>Series</span>
+                    </a>
+                </NavMenu>
+
+                <UserImg src='https://images.mubicdn.net/images/cast_member/2184/cache-2992-1547409411/image-w856.jpg?size=800x'/>
+            </Nav>
     );
 };
 
 export default Header;
+
+const Nav=styled.nav`
+       height:70px;
+       background:#090b13;
+       display:flex;
+       align-items:center;
+       padding:0 36px;
+`
+
+const Logo=styled.img`
+        height:60px;
+`
+
+const NavMenu=styled.div`
+    display:flex;
+    flex:1;
+    margin-left: 25px;
+    align-items:center;
+
+      a{
+        display:flex;
+        align-items:center;
+        padding:0px 12px;
+        cursor:pointer;
+
+        img{
+            height:20px;
+        }
+
+        span{
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            position:relative;
+            
+            &:after{
+                content:"";
+                height:2px;
+                background:white;
+                position:absolute;
+                left:0;
+                right:0;
+                bottom:-6px;
+                opacity:0;
+                transform-origin:left center;
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform:scaleX(0);
+            }
+        }
+        &:hover{
+            span:after{
+                opacity:1;
+                transform: scaleX(1);
+            }
+        }
+      }
+`
+
+const UserImg=styled.img`
+       height: 48px;
+       width: 48px;
+       border-radius: 50%;
+       cursor:pointer;
+`
